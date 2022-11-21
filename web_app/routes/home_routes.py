b08c3ed41955311e00572ced2ed32819fa35aa7c
rayhas2work <73216882+rayhas2work@users.noreply.github.com>
2022-11-21 00:21:49 -0500

    A

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -8,14 +8,14 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
-    #return render_template("home.html")
+    #return "Welcome Home"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
-    #return render_template("about.html")
+    #return "About Me"
+    return render_template("about.html")
 
 @home_routes.route("/hello")
 def hello_world():
